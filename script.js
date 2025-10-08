30ecc908d30f6c30b16c6c315b0b04abce537622
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-08 13:24:09 -0600

    Add function and variable for number buttons

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict"
+
+const numButtons = document.querySelectorAll("#number-buttons button");
+
+let screenResult = document.querySelector("#screen-result")
+
+const getNumButtonOutput = () => {
+    numButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        screenResult.textContent = button.textContent; 
+    })
+    })
+};
+
+getNumButtonOutput()
