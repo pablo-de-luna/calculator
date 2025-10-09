5187743941499a41b7c6615062e86ccbb4697913
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-08 18:59:08 -0600

    Add sum funcion and equal sign button

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 "use strict"
 
-const screenResult = document.querySelector("#screen-result")
+const screen = document.querySelector("#screen")
 const numberButtons = document.querySelectorAll("#number-buttons button");
 const operatorButtons = document.querySelectorAll("#operator-buttons button");
+const equalButton = document.querySelector("#equal-button");
 const allButtons = document.querySelectorAll("#buttons-layout div button")
+
+let firstValue = "";
+let secondValue = ""; 
+let operator = "";
+let result;
 
 const getValues = () => {
     numberButtons.forEach(button => {
         button.addEventListener("click", () => {
             if (!operator) {
                 firstValue += button.textContent;
-                console.log("first = " + firstValue);
             }
             if (operator) {
                 secondValue += button.textContent;
-                console.log("second = " + secondValue);
             }
         });
     });
@@ -28,19 +32,28 @@ const getOperator = () => {
     });
 };
 
-let firstValue = "";
-let secondValue = ""; 
-let operator = "";
-let result;
+const operate = () => {
+    equalButton.addEventListener("click", () => {
+        getSum()
+    });
+};
+
+const getSum = () => {
+    result = Number(firstValue) + Number(secondValue);
+};
 
 const updateScreen = () => {
     allButtons.forEach(button => {
         button.addEventListener("click", () => {
-            screenResult.textContent = `${firstValue} ${operator} ${secondValue}`;
+            screen.textContent = `${firstValue} ${operator} ${secondValue}`;
         });
+    });
+    equalButton.addEventListener("click", () => {
+        screen.textContent = result;
     });
 };
 
 getValues();
 getOperator();
+operate();
 updateScreen();
